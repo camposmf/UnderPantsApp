48230e3bc357d2605cadb9a1bc597a34730fb90a
Marcos Campos <marcos.campos@wtwco.com>
2023-10-02 09:29:41 -0300

    improvement: configurando API para se conectar com banco de dados.

--- backend/src/Program.cs
+++ backend/src/Program.cs
@@ -1,23 +1,32 @@
-using Serilog;
 using System.Text;
 using System.Reflection;
+using UnderPantsApp.Services;
+using UnderPantsApp.DbContexts;
+using UnderPantsApp.Repository;
 using Microsoft.OpenApi.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.AspNetCore.StaticFiles;
-
-Log.Logger = new LoggerConfiguration()
-    .MinimumLevel.Debug()
-    .WriteTo.Console()
-    .WriteTo.File("Logs/under-pants.txt", rollingInterval: RollingInterval.Day)
-    .CreateLogger();
+using UnderPantsApp;
 
 var builder = WebApplication.CreateBuilder(args);
-//builder.Host.UseSerilog();
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    options.ReturnHttpNotAcceptable = true;
+}).AddNewtonsoftJson()
+  .AddXmlDataContractSerializerFormatters();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<FileExtensionContentTypeProvider>();
+
+builder.Services.AddDbContext<UnderPantsContext>(
+    dbContextOptions => dbContextOptions.UseSqlite(builder.Configuration["ConnectionStrings:UnderPantsDB"])
+);
+
+builder.Services.AddScoped<IUserRepository, UserService>();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
