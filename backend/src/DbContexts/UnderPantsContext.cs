632d770c6ea11bf9d4616ef321993a7cebc01eb8
Marcos Campos <marcos.campos@wtwco.com>
2023-10-02 09:25:07 -0300

    criation: criação do modulo de injeção de depência do banco de dados

---
+++ backend/src/DbContexts/UnderPantsContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UnderPantsApp.Entity;
+
+namespace UnderPantsApp.DbContexts
+{
+    public class UnderPantsContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Income> Incomes { get; set; }
+        public DbSet<Expense> Expenses { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<SavingsGoal> SavingsGoals { get; set; }
+        public DbSet<RecurringExpense> RecurringExpenses { get; set; }
+        public DbSet<FinancialHistory> FinancialHistories { get; set; }
+
+        public UnderPantsContext(DbContextOptions<UnderPantsContext> option) : base(option) { }
+        
+    }
+}
