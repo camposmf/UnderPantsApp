a416bbff4606bd2369a75689b7f01e064efda42e
Marcos Campos <marcos.campos@wtwco.com>
2023-10-02 09:26:38 -0300

    criation: criação da classe para adicionar e editar usuários

---
+++ backend/src/Models/User/UserForCreationModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace UnderPantsApp.Models.User
+{
+    public class UserForCreationModel
+    {
+        [MaxLength(30)]
+        [Required(ErrorMessage = "Você deve fornecer um valor para o login do usuário.")]
+        public string Login { get; set; } = string.Empty;
+
+        [MaxLength(20)]
+        [Required(ErrorMessage = "Você deve fornecer um valor para a senha do usuário.")]
+        public string Password { get; set; } = string.Empty;
+
+        [MaxLength(100)]
+        [Required(ErrorMessage = "Você deve fornecer um nome para o usuário.")]
+        public string Name { get; set; } = string.Empty;
+
+        [MaxLength(100)]
+        [Required(ErrorMessage = "Você deve fornecer um email para o usuário.")]
+        public string Email { get; set; } = string.Empty;
+    }
+}
---
+++ backend/src/Models/User/UserForUpdateModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UnderPantsApp.Models.User
+{
+    public class UserForUpdateModel
+    {
+        [MaxLength(30)]
+        [Required(ErrorMessage = "Você deve fornecer um valor para o login do usuário.")]
+        public string Login { get; set; } = string.Empty;
+
+        [MaxLength(20)]
+        [Required(ErrorMessage = "Você deve fornecer um valor para a senha do usuário.")]
+        public string Password { get; set; } = string.Empty;
+
+        [MaxLength(100)]
+        [Required(ErrorMessage = "Você deve fornecer um nome para o usuário.")]
+        public string Name { get; set; } = string.Empty;
+
+        [MaxLength(100)]
+        [Required(ErrorMessage = "Você deve fornecer um email para o usuário.")]
+        public string Email { get; set; } = string.Empty;
+    }
+}
