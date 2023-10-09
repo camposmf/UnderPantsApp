e7fde14053e621b53954a6c9b113c30999ce076b
Marcos Campos <marcos.campos@wtwco.com>
2023-10-09 03:10:05 -0300

    bugfix: corrigindo entidade de histórico financeiro

--- backend/src/Entity/FinancialHistory.cs
+++ backend/src/Entity/FinancialHistory.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace UnderPantsApp.Entity
@@ -13,5 +12,21 @@ namespace UnderPantsApp.Entity
         [ForeignKey("UserId")]
         public User? User { get; set; }
         public int UserId { get; set; }
+
+        [ForeignKey("ExpenseId")]
+        public Expense? Expense { get; set; }
+        public int ExpenseId { get; set; }
+
+        [ForeignKey("IncomeId")]
+        public Income? Income { get; set; }
+        public int IncomeId { get; set; }
+
+        [ForeignKey("AccountId")]
+        public Account? Account { get; set; }
+        public int AccountId { get; set; }
+
+        [ForeignKey("SavingsGoalId")]
+        public SavingsGoal? SavingsGoal { get; set; }
+        public int SavingsGoalId { get; set; }
     }
 }
--- backend/src/Models/FinancialHistory/FinancialHistoryModel.cs
+++ backend/src/Models/FinancialHistory/FinancialHistoryModel.cs
@@ -1,4 +1,8 @@
 ﻿using UnderPantsApp.Models.User;
+using UnderPantsApp.Models.Income;
+using UnderPantsApp.Models.Account;
+using UnderPantsApp.Models.Expense;
+using UnderPantsApp.Models.SavingsGoal;
 
 namespace UnderPantsApp.Models.FinancialHistory
 {
@@ -6,5 +10,9 @@ namespace UnderPantsApp.Models.FinancialHistory
     {
         public int Id { get; set; }
         public UserModel UserId { get; set; } = new UserModel();
+        public IncomeModel Income { get; set; } = new IncomeModel();
+        public AccountModel account { get; set; } = new AccountModel();
+        public ExpenseModel Expense { get; set; } = new ExpenseModel();
+        public SavingsGoalModel SavingsGoal { get; set; } = new SavingsGoalModel();
     }
 }
