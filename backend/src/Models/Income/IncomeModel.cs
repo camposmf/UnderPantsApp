484b6e4ac10e90724d54f07beeaa98b385e39d68
Marcos Campos <marcos.campos@wtwco.com>
2023-10-09 03:03:33 -0300

    bugfix: corrigindo entidade de receita

--- backend/src/Models/Income/IncomeModel.cs
+++ backend/src/Models/Income/IncomeModel.cs
@@ -5,9 +5,8 @@ namespace UnderPantsApp.Models.Income
     public class IncomeModel
     {
         public int Id { get; set; }
-        public FinancialHistoryModel HistoryId { get; set; } = new FinancialHistoryModel();
-        public string Description { get; set; } = string.Empty;
         public double Amount { get; set; }
         public DateTime Date { get; set; }
+        public string Description { get; set; } = string.Empty;
     }
 }
