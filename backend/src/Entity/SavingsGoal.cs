2ee46115487c0dd434e82aeb335523eff9d80cc5
Marcos Campos <marcos.campos@wtwco.com>
2023-10-09 03:05:39 -0300

    bugfix: corrigindo entidade de metas

--- backend/src/Entity/SavingsGoal.cs
+++ backend/src/Entity/SavingsGoal.cs
@@ -9,18 +9,14 @@ namespace UnderPantsApp.Entity
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
-        [ForeignKey("HistoryId")]
-        public FinancialHistory? History { get; set; }
-        public int HistoryId { get; set; }
-
-        [Required]
-        [MaxLength(255)]
-        public string Description { get; set; } = string.Empty;
-
         [Required]
         public double GoalAmount { get; set; }
 
         [Required]
         public DateTime CompletionDate { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Description { get; set; } = string.Empty;
     }
 }
--- backend/src/Models/SavingsGoal/SavingsGoalModel.cs
+++ backend/src/Models/SavingsGoal/SavingsGoalModel.cs
@@ -1,13 +1,10 @@
-﻿using UnderPantsApp.Models.FinancialHistory;
-
-namespace UnderPantsApp.Models.SavingsGoal
+﻿namespace UnderPantsApp.Models.SavingsGoal
 {
     public class SavingsGoalModel
     {
         public int Id { get; set; }
-        public FinancialHistoryModel HistoryId { get; set; } = new FinancialHistoryModel();
-        public string Description { get; set; } = string.Empty;
-        public double GoalAmount { get; set; }
         public DateTime Date { get; set; }
+        public double GoalAmount { get; set; }
+        public string Description { get; set; } = string.Empty;
     }
 }
